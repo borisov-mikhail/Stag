04c6608be1676cef1fa2a412219da8f9659079fc
Mikhail Borisov <travka_mx@mail.ru>
2018-08-27 00:15:39 +0300

    working algoritm

--- cgi-bin/message.py
+++ cgi-bin/message.py
@@ -6,14 +6,19 @@ form = cgi.FieldStorage()
 
 
 def get_dividers(user_number):
-    dividers = []
-    divider = 2
-    while user_number != 1:
-        if user_number % divider == 0:
-            dividers.append(divider)
-            user_number /= divider
-        else:
-            divider += 1
+    if user_number > 2:
+        dividers = ' '
+        divider = 2
+        while user_number != 1:
+            if user_number % divider == 0:
+                dividers = dividers + str(divider) + ' '
+                user_number /= divider
+            else:
+                divider += 1
+    elif user_number == 1 or user_number == 0:
+        dividers = user_number
+    else:
+        dividers = 'ERROR'
     return dividers
 
 
@@ -21,6 +26,6 @@ print("Content-type: text/html")
 
 value = form.getfirst("user_number", 0)
 
-list_1 = get_dividers(value)
+list_1 = get_dividers(int(value))
 
-print("\n<h1>Dividers: {}</h1>".format(list_1[1]))
+print("\n<h1>Dividers: {}</h1>".format(list_1))
--- main_divide.py
+++ main_divide.py
@@ -2,19 +2,22 @@ import math
 
 
 def get_dividers(user_number):
-    dividers = []
-    divider = 2
-    while user_number != 1:
-        if user_number % divider == 0:
-            dividers.append(divider)
-            user_number /= divider
-        else:
-            divider += 1
+    if user_number > 2:
+        dividers = ' '
+        divider = 2
+        while user_number != 1:
+            if user_number % divider == 0:
+                dividers = dividers + str(divider) + ' '
+                user_number /= divider
+            else:
+                divider += 1
+    else:
+        dividers = 'ERROR'
     return dividers
 
 
-print(get_dividers(125))
-print(get_dividers(51))
-print(get_dividers(5165))
+print(get_dividers(1.25))
+print(get_dividers(51.0))
+print(get_dividers(51.65))
 print(get_dividers(1001))
 print(get_dividers(66))
